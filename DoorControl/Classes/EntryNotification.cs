d1ace215772f46e351676f53f593be255dfc11c1
andersKallesoee <anderskallesoee@gmail.com>
2021-02-25 13:57:58 +0100

    Added folders to solution

---
+++ DoorControl/Classes/EntryNotification.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DoorControl.Classes
+{
+    class EntryNotification
+    {
+    }
+}
---
+++ DoorControl/Interfaces/INotification.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DoorControl.Interfaces
+{
+    interface INotification
+    {
+    }
+}
